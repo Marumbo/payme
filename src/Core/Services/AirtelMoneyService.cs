710871f8bd8236fc0106fa73ff0f67f9342ce4bd
Marumbo <marumbok@gmail.com>
2020-08-27 19:17:30 +0200

    added more logic to the deposit test for 3 sample airtel messages on submitting

--- src/Core/Services/AirtelMoneyService.cs
+++ src/Core/Services/AirtelMoneyService.cs
@@ -47,13 +47,13 @@ namespace Core.Services
                       /* sample messages for the regex below
                         var message = @" Trans ID: CI200808.0942.H11201: you have received MK20000.00 from BT59687, CHIMWEMWE MUGHOGHO. your new balance is MK20020.41.Note that Cash In is free of charge";
                         var message2 = "trans.ID :  PP200705.1009.H42097. Dear customer, you have received MK 6000.00 from 999025907,PRINCE CHIKWEBA . Your available balance is MK 6020.41.Trans id";
-                        var message3 = "Dear Customer, money transfer to 999057130, ELIZABETH JERE is successful, Trans Id: PP200815.0822.G60250,Trans Amt: MK2000.00. Your available balance MK110.41";
+                        var message3 = "Dear Customer, money transfer to 999057130, Dali Mizaya is successful, Trans Id: PP200815.0822.G60250,Trans Amt: MK2000.00. Your available balance MK110.41";
 
                         */
                         
                         var findFirst = new Regex(@"(^( [tT]rans ID: |[tT]rans ID: |[Tt]rans.ID|Trans Id:|Dear Customer))");
                         var airtimeRegex = new Regex(@"[aA]irtime");
-                        var findReceived = new Regex(@"(\b(money transfer)|(you have received)\b)");
+                        var findReceived = new Regex(@"(\b(money transfer)|(you have received)|(successful)\b)");
                         var transIdRegex = new Regex(@"(?<=([tT]rans.ID :)|([tT]rans ID:|Trans Id:))(.*?)(?=([dD]|[yY]|:|,))");
                         var amountRegex = new Regex(@"((?<=(Amt: MK |received MK|Trans Amt: MK))(.*?)(?=\.|[yY]|f))");
                         var senderRegex = new Regex(@"(?<=(from |to ))(.*?)(?=\.|is)");
@@ -77,13 +77,24 @@ namespace Core.Services
 
                         /*test for airtel trans id and dear customer only in airtel messages and no airtime */
                          
-                        if(firstWord != "" && receivedMessage != "" && airtime == "")
-                        {
+                         if(firstWord == "dear customer" && receivedMessage != "" && name == "Dali Mizaya")
+                       {
+                           
+
                             return true;
                         }
+                       
+                        if (firstWord != "" && receivedMessage != "" && airtime == "")
+                        {
+                            
+                            return true;
+                        }
+
                         else 
+                        {
                             return false;
 
+                        }
         }
     }
 }
